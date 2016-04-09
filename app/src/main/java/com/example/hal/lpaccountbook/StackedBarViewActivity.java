7ea159ef906fc0e38dbd12534ccc00d00cf91200
monohal <monomonocasino@gmail.cp>
2016-04-10 02:12:36 +0900

    StackedBar正常動いた

--- app/src/main/java/com/example/hal/lpaccountbook/StackedBarViewActivity.java
+++ app/src/main/java/com/example/hal/lpaccountbook/StackedBarViewActivity.java
@@ -23,90 +23,95 @@ public class StackedBarViewActivity extends Activity{
 
     HorizontalBarChart sbChart;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_stackedbar);
-
-        sbChart = (HorizontalBarChart) findViewById(R.id.chart1);
-        sbChart.setDescription("");
-
-        sbChart.setMaxVisibleValueCount(60);
-        sbChart.setDrawGridBackground(false);
-        sbChart.setDrawBarShadow(false);
-        sbChart.setDrawValueAboveBar(false);
 
         DataBaseHelper helper = new DataBaseHelper(getApplicationContext());
         SQLiteDatabase sqdb = helper.getReadableDatabase();
         SectionManage sm = new SectionManage(this);
 
-        int i;
+        sbChart = (HorizontalBarChart) findViewById(R.id.chart1);
 
-        String col[] = new String[]{Data._ID, "TOTAL(" + Data.MONEY_DATA +")", Data.STRING_DATA, Data.M_YMD_DATA, Data.S_YMD_DATA};
+        sbChart.setDescription("");
+        sbChart.setMaxVisibleValueCount(60);
+        sbChart.setDrawGridBackground(false);
+        sbChart.setDrawBarShadow(false);
+        sbChart.setDrawValueAboveBar(false);
 
+        ArrayList<BarEntry> yVals1 = new ArrayList<BarEntry>();
+        ArrayList<Integer> colors = new ArrayList<>();
 
+        int mDateMin = Integer.MAX_VALUE;
+        int mDateMax = Integer.MIN_VALUE;
+        String sDateMin = null;
+        String sDateMax = null;
+
+        ArrayList<String> xVals = new ArrayList<String>();
+        float[] fDataArray;
+        String[] sDataArray = null;
+
+        String col[] = new String[]{Data._ID, "TOTAL(" + Data.MONEY_DATA +")"
+                , Data.STRING_DATA, Data.M_YMD_DATA, Data.S_YMD_DATA};
 
         for(int j = 0; j < sm.getNowSection(); j++ ) {
             Cursor cur = sqdb.query(
-                    Database.TABLE_NAME,       //テーブル名
-                    col,            //カラム名の配列
-                    Data.SECTION_DATA + "=?",           //取得するレコードの条件
-                    new String[]{String.valueOf(sm.getNowSection())},           //取得するレコードの条件
+                    Database.TABLE_NAME,        //テーブル名
+                    col,                        //カラム名の配列
+                    Data.SECTION_DATA + "=?",   //取得するレコードの条件
+                    new String[]{String.valueOf(sm.getNowSection() - j)},       //取得するレコードの条件
                     Data.STRING_DATA,           //GroupBy
-                    null,           //Having
+                    null,                       //Having
                     "TOTAL(" + Data.MONEY_DATA + ")" + " Asc");          //orderBy
 
-            String[] xVals = new String[cur.getCount()];
-            float[] yVals = new float[cur.getCount()];
-            ArrayList<BarEntry> yVals1 = new ArrayList<>();
-            ArrayList<Integer> colors = new ArrayList<>();
+            fDataArray = new float[cur.getCount()];
+            sDataArray = new String[cur.getCount()];
 
-            int mDateMin = Integer.MAX_VALUE;
-            int mDateMax = Integer.MIN_VALUE;
-            String sDateMin = null;
-            String sDateMax = null;
 
             cur.moveToFirst();
-
-            for (i = 0; i < cur.getCount(); i++) {     //query結果
+            for (int i = 0; i < cur.getCount(); i++) {    //query結果
                 Log.d("OUTPUT", String.valueOf(cur.getInt(0)));
                 Log.d("OUTPUT", String.valueOf(cur.getInt(1)));
                 Log.d("OUTPUT", cur.getString(2));
-                Log.d("OUTPUT", "--------------------");
+                Log.d("OUTPUT", String.valueOf(cur.getInt(3)));
+                Log.d("OUTPUT", cur.getString(4));
+                Log.d("OUTPUT", "-------");
 
-                yVals[i] = Float.valueOf(cur.getInt(1));
-                xVals[i] = cur.getString(2);
+                fDataArray[i] = Float.valueOf(cur.getInt(1));
+                sDataArray[i] = cur.getString(2);
+                cur.getString(2);
                 colors.add(Color.parseColor(Data.COLOR_DATA[i % 4]));
 
-
+                //日付最小値と最大値を得る
                 if (cur.getInt(3) < mDateMin) {
                     sDateMin = cur.getString(4);
                 }
-
                 if (cur.getInt(3) > mDateMax) {
                     sDateMax = cur.getString(4);
                 }
-
                 cur.moveToNext();
             }
 
-            Log.d("OUTPUT", mDateMin + "---" + mDateMax);
-
-            yVals1.add(new BarEntry(yVals, 0));
-            BarDataSet set1 = new BarDataSet(yVals1, "Element");
-            set1.setColors(colors);
-            set1.setStackLabels(xVals);
-
-            ArrayList<IBarDataSet> dataSets = new ArrayList<>();
-            dataSets.add(set1);
-
-            BarData barData = new BarData(new String[]{sDateMin + "～" + sDateMax}, dataSets);
-            sbChart.setData(barData);
+            yVals1.add(new BarEntry(fDataArray, j));
+            xVals.add(sDateMin +"～"+ sDateMax);
+            Log.d("OUTPUT", "--------------------");
         }
 
+        BarDataSet set1 = new BarDataSet(yVals1, "");
+        set1.setColors(colors);
+        set1.setStackLabels(sDataArray);
+
+        ArrayList<IBarDataSet> dataSets = new ArrayList<IBarDataSet>();
+        dataSets.add(set1);
+
+        BarData data = new BarData(xVals, dataSets);
+        data.setValueFormatter(new MyValueFormatter());
+
+        sbChart.setData(data);
         sbChart.invalidate();
     }
 }
